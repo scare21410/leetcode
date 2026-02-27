313596ec45ec9e1c80e04d3b5329e2e923664711
Martin Komara <martin.komara@gmail.com>
2026-02-27 21:26:04 +0100

    Added 0007 solution (ts)

---
+++ problems/0007-reverse-integer/solution.ts
@@ -0,0 +1,18 @@
+const MIN_INT_DIV_10 = -((Math.pow(2, 31) / 10) >> 0);
+const MAX_INT_DIV_10 = ((Math.pow(2, 31) - 1) / 10) >> 0;
+
+export function reverse(x: number): number {
+    let result = 0;
+    let remainder = x;
+    while (remainder !== 0) {
+        // check for overflow before multiplying
+        if (result < MIN_INT_DIV_10 || result > MAX_INT_DIV_10) {
+            return 0;
+        }
+        result *= 10;
+        result += remainder % 10;
+        remainder = (remainder / 10) >> 0;
+    }
+
+    return result;
+};
